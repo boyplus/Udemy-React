85f1c186043fdf653c78fef9039fbbb34a2fd451
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-01-18 15:43:24 +0700

    [UPDATE] redux lecture

--- Redux.md
+++ Redux.md
@@ -17,7 +17,77 @@
 - Dispatch (form reciver)
 - Reducers (Departments)
 - State (Compiled department data)
-- 
+
+
+
+**1.Action creators**
+
+-> the function which return palin javascript object
+
+```javascript
+const createPolicy = (name,amount) =>{
+  return {
+    //action (javascript object)
+  };
+}
+```
+
+
+
+
+
+**2.Action**
+
+->  it is the **object** which represents things that we want to do and data in it.
+
+-> consits of two parts
+
+- **type**  (type of action Ex. CREATE_POLICY, we usually make it uppercase)
+- **payload**  (data in action ex. name, amount)
+
+
+
+**3.Dispatch**
+
+-> to update the data in store which pass argument of action creatots
+
+-> we usually do this in the last step, because we have to write the action creators, actions, reducers, combineReducers and create the store
+
+```javascript
+const store = createStore(ourDepartments);
+store.dispatch(createPolicy('Alex',20));
+console.log(store.getState());
+```
+
+
+
+**4.Reducers**
+
+-> it's the modifier function to change and return the new data
+
+-> it recieve two arguments, first one is the old data from store and second one is action (form)
+
+-> we have to check what type of the action, then we consider what we will do about that type of action
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- redux-example.js
+++ redux-example.js
@@ -1,83 +1,77 @@
 console.clear();
 
 //People dropping off a form (Action creator)
-//the function whioch return plain javascript object
-const createPolicy = (name,amount) =>{
-  return {
-    //what we will return is action (the form)
-    type: 'CREATE_POLICY',
-    payload: {
-      name: name,
-      amount: amount
-    }
-  };
+//the function which return plain javascript object
+const createPolicy = (name, amount) => {
+    return {
+        //what we will return is action (the form)
+        type: 'CREATE_POLICY',
+        payload: {
+            name: name,
+            amount: amount
+        }
+    };
 };
-const deletePolicy = (name) =>{
-  return {
-    type: 'DELETE_POLICY',
-    payload:{
-      name : name
-    }
-  };
+const deletePolicy = name => {
+    return {
+        type: 'DELETE_POLICY',
+        payload: {
+            name: name
+        }
+    };
 };
-const createClaim = (name,amountOfMoneyToCollect) =>{
-  return {
-    type: 'CREATE_CLAIM',
-    payload:{
-      name: name,
-      amountOfMoneyToCollect: amountOfMoneyToCollect
-    }
-  };
+const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+        type: 'CREATE_CLAIM',
+        payload: {
+            name: name,
+            amountOfMoneyToCollect: amountOfMoneyToCollect
+        }
+    };
 };
 
-//Reducer (Departments) -> the modifier function
+//Reducer (Departments) -> the modifier function to change return the new data
 const claimsHistory = (oldListOfClaims = [], action) => {
-  if(action.type === 'CREATE_CLAIM'){
-    //we care about yhis action (form)
-    //[...someArray] -> include someArray in brand new array
-    //we did not modify the old array, we return the brand new array
-    return [...oldListOfClaims, action.payload];
-  }
-  //we do not care about the action (form)
-  return oldListOfClaims;
+    if (action.type === 'CREATE_CLAIM') {
+        //we care about yhis action (form)
+        //[...someArray] -> include someArray in brand new array
+        //we did not modify the old array, we return the brand new array
+        return [...oldListOfClaims, action.payload];
+    }
+    //we do not care about the action (form)
+    return oldListOfClaims;
 };
 
-
-const accounting = (bagOfMoney = 100, action) =>{
-  if(action.type === 'CREATE_CLAIM'){
-    return bagOfMoney-action.payload.amountOfMoneyToCollect;
-  }
-  else if(action.type === 'CREATE_POLICY'){
-    return bagOfMoney + action.payload.amount;
-  }
-  return bagOfMoney;
+const accounting = (bagOfMoney = 100, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return bagOfMoney - action.payload.amountOfMoneyToCollect;
+    } else if (action.type === 'CREATE_POLICY') {
+        return bagOfMoney + action.payload.amount;
+    }
+    return bagOfMoney;
 };
 
-
-const policies = (listOfPolicies = [], action) =>{
-  if(action.type === 'CREATE_POLICY'){
-    return [...listOfPolicies,action.payload.name];
-  }
-  else if(action.type === 'DELETE_POLICY'){
-    return listOfPolicies.filter(name=> name!==action.payload.name);
-  }
-  return listOfPolicies;
-}
-
-
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload.name];
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolicies.filter(name => name !== action.payload.name);
+    }
+    return listOfPolicies;
+};
 
 console.log(Redux);
-const {createStore, combineReducers} = Redux;
+const { createStore, combineReducers } = Redux;
 
 //combine the reducer together
-const ourDepoartments = combineReducers({
-  accounting: accounting,
-  policies: policies,
-  claimsHistory: claimsHistory
+const ourDepartments = combineReducers({
+    accounting: accounting,
+    policies: policies,
+    claimsHistory: claimsHistory
 });
 
 //create the store (all data)
-const store = createStore(ourDepoartments);
+const store = createStore(ourDepartments);
 store.dispatch(createPolicy('Alex', 20));
 store.dispatch(createPolicy('Jim', 30));
 store.dispatch(createPolicy('Bob', 15));
@@ -86,7 +80,6 @@ store.dispatch(createClaim('Alex', 20));
 store.dispatch(createClaim('Jim', 50));
 
 store.dispatch(deletePolicy('Bob'));
-
 
 //getState will return the all data
 console.log(store.getState());
